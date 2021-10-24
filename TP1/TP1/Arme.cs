94ec275c4021d90ec06582c5412a8e793d7a6a5d
HAMMBE <benjamin.hamm7@gmail.com>
2021-10-24 09:33:27 +0200

    feat: Update arme

--- TP1/TP1/Arme.cs
+++ TP1/TP1/Arme.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP1;
 
 namespace TP1
 {
     class Arme
     {
+        private string nom {get; set;}
+        private int degatMin {get; set;}
+        private int degatMax {get; set;}
+
+        public Arme(string nom, int degatMin,int degatMax)
+        {
+            this.nom = nom;
+            this.degatMin = degatMin;
+            this.degatMax = degatMax;
+        }
     }
 }
