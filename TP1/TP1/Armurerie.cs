4b5f4c85fccb683b3c18f11736897f5b0207bf4d
HAMMBE <benjamin.hamm7@gmail.com>
2021-10-24 09:51:19 +0200

    feat: Add Armurerie

---
+++ TP1/TP1/Armurerie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP1;
+
+namespace TP1
+{
+    class Armurerie
+    {
+        private List<Arme> Armes {get; set;}
+
+        public Armurerie()
+        {
+            Init();
+        }
+        private void Init()
+        {
+            Armes = new List<Arme>();
+            Armes.Add(new Arme("AK-47", 100, 200, Type.Direct));
+            Armes.Add(new Arme("C4", 50, 1000, Type.Explosif));
+            Armes.Add(new Arme("Lance-Roquette", 700, 1000, Type.Guidé));
+        }
+    }
+}
