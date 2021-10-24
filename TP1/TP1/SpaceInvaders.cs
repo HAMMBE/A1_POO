d1f08b9aa1b510372374adacbab5fe5136b167aa
HAMMBE <benjamin.hamm7@gmail.com>
2021-10-24 09:14:32 +0200

    feat: Add SpaceInvaders

--- TP1/TP1/SpaceInvaders.cs
+++ TP1/TP1/SpaceInvaders.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TP1;
 
 namespace TP1
 {
     class SpaceInvaders
     {
+        private List<Joueur> joueurs;
+        public SpaceInvaders(){
+            joueurs = Init();
+        }
+
+        private List<Joueur> Init()
+        {
+            List<Joueur> joueurs = new();
+            joueurs.Add(new Joueur("Benjamin", "Hamm", "ESAqua"));
+            joueurs.Add(new Joueur("Amine", "Mersel", "AbsoToasty"));
+            joueurs.Add(new Joueur("Noel", "Larcher", "yuubeats"));
+
+            return joueurs;
+        }
+
+        static void Main(string[] args)
+        {
+            SpaceInvaders jeu = new SpaceInvaders();
+
+
+            foreach (var j in jeu.joueurs)
+            {
+                Console.WriteLine(j.ToString());
+            }
+        }
     }
 }
